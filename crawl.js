3ea892ccd372a4bad474ee56f5123a6648d4814c
aliaitrahou <145855773+aliaitrahou@users.noreply.github.com>
2024-03-30 12:19:05 +0000

    fixing bugs

--- crawl.js
+++ crawl.js
@@ -1,29 +1,52 @@
 const { JSDOM } = require("jsdom"); // use this lib to convert the html string to real document object modele
 // so we can grap the a tag and accses the href
 
-async function crawlPage(currentUrl) {
-  console.log(`actively crawling: ${currentUrl}`);
-  try {
-    const resp = await fetch(currentUrl);
-    if (resp.status > 399) {
-      console.log(
-        `error in fetch with status code: ${resp.status}, on page : ${currentUrl}`,
-      );
-      return;
-    }
-    const typeOfContent = resp.headers.get("content-type");
-    if (!typeOfContent.includes("text/html")) {
-      console.log(
-        `non html response, content type: ${typeOfContent}, on page ${currentUrl}`,
-      );
-      return;
-    }
-    console.log(await resp.text());
-  } catch (err) {
-    console.log(
-      "we have a problem which is :" + err.message + "and " + err.status,
-    );
+async function crawlPage(baseURL, currentURL, pages) {
+  // if this is an offsite URL, bail immediately
+  const currentUrlObj = new URL(currentURL);
+  const baseUrlObj = new URL(baseURL);
+  if (currentUrlObj.hostname !== baseUrlObj.hostname) {
+    return pages;
   }
+
+  const normalizedURL = getNormalURL(currentURL);
+  // if we've already visited this page
+  // just increase the count and don't repeat
+  // the http request
+  if (pages[normalizedURL] > 0) {
+    pages[normalizedURL]++;
+    return pages;
+  }
+
+  // initialize this page in the map
+  // since it doesn't exist yet
+  pages[normalizedURL] = 1;
+
+  // fetch and parse the html of the currentURL
+  console.log(`crawling ${currentURL}`);
+  let htmlBody = "";
+  try {
+    const resp = await fetch(currentURL);
+    if (resp.status > 399) {
+      console.log(`Got HTTP error, status code: ${resp.status}`);
+      return pages;
+    }
+    const contentType = resp.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(`Got non-html response: ${contentType}`);
+      return pages;
+    }
+    htmlBody = await resp.text();
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+  for (const nextURL of nextURLs) {
+    pages = await crawlPage(baseURL, nextURL, pages);
+  }
+
+  return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { crawlPage } = require("./crawl.js");
-const main = () => {
+
+const main = async () => {
   if (process.argv.length < 3) {
     console.log("not website provided");
     process.exit(1);
@@ -10,6 +11,10 @@ const main = () => {
   }
   const baseURL = process.argv[2];
   console.log(`start crawling of ${baseURL}`);
-  crawlPage(baseURL);
+  const pages = await crawlPage(baseURL, baseURL, {});
+  for (const page of Object.entries(pages)) {
+    console.log(page);
+  }
 };
+
 main();
